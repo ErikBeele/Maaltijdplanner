6df0538d96461f5630c2624cfef444dbcf0fddd4
Don332 <175736@student.horizoncollege.nl>
2024-05-22 14:29:44 +0200

    index.php 2 images added
    index.php 2 images added

--- index.php
+++ index.php
@@ -9,13 +9,15 @@
 <body>
    <header>
     <nav>
-        <img src="logo.png" alt="logo">
+        <img src="logo.png" alt="websitelogo">
         <ul>
             <li><a href="#">Gerechten</a></li>
             <li><a href="#">Maaltijden</a></li>
             <li><a href="#">Aanbiedingen</a></li>
             <li><a href="#">klantenservice</a></li>
         </ul>
+        <img src="list.png" alt="yourlist">
+        <img src="profile.png" alt="profile">
     </nav>
    </header>
 </body>
