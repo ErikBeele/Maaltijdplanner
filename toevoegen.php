6128c5ac4f27a9d04a9522f84673b6a2502abb85
ErikBeelen <eriklbeelen@gmail.com>
2024-06-12 12:51:12 +0200

    gerecht toevoegen toegevoegt
    type: create
    scope toevoegen.php
    description: Toevoegen.php toegevoegt

--- toevoegen.php
+++ toevoegen.php
@@ -1,6 +1,17 @@
-<?php
+<<html>
+    <form action="verwerk.php" method="POST">
+        <label for="titel">Titel:</label><br>
+        <input type="text" id="titel" name="titel"><br>
+        <label for="gerecht">Soort gerecht:</label><br>
+        <input type="text" id="gerecht" name="gerecht"><br>
+        <label for="duur">Benodigde tijd:</label><br>
+        <input type="int" id="tijd" name="tijd"><br>
+        <label for="ingrediënt">Ingrediënten:</label><br>
+        <input type="text" id="ingrediënt" name="ingrediënt"><br>
+        <label for="rating">Rating:</label><br>
+        <input type="text" id="rating" name="rating"><br>
+        <input type="hidden" id="id" name="id"><br>
+        <input type="submit" value="submit">
+    </form>
 
-
-
-
-?>
+</html>
