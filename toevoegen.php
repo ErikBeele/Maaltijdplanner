9aa0cfed4a3eecea4c91a7b3a7bdceb9d1ee1ad9
ErikBeelen <eriklbeelen@gmail.com>
2024-06-18 10:38:16 +0200

    ingrediënten toevoegen.php
    type: update
    scope: form toevoegen.php
    description: Ingrediënten in lijst op toevoegen.php toegevoegd.

--- toevoegen.php
+++ toevoegen.php
@@ -28,8 +28,18 @@
         </select><br>
         <label for="duur">Benodigde tijd in minuten:</label><br>
         <input type="int" id="tijd" name="tijd"><br>
-        <label for="ingrediënt">Ingrediënten:</label><br>
+        <label for="ingrediënt">Ingrediënt 1:</label><br>
         <input type="text" id="ingrediënt" name="ingrediënt"><br>
+        <label for="ingrediënt2">Ingrediënt 2:</label><br>
+        <input type="text" id="ingrediënt2" name="ingrediënt2"><br>
+        <label for="ingrediënt3">Ingrediënt 3:</label><br>
+        <input type="text" id="ingrediënt3" name="ingrediënt3"><br>
+        <label for="ingrediënt4">Ingrediënt 4:</label><br>
+        <input type="text" id="ingrediënt4" name="ingrediënt4"><br>
+        <label for="ingrediënt">Ingrediënt 5:</label><br>
+        <input type="text" id="ingrediënt5" name="ingrediënt5"><br>
+        <label for="ingrediënt6">Ingrediënt 6:</label><br>
+        <input type="text" id="ingrediënt6" name="ingrediënt6"><br>
         <label for="rating">Rating:</label><br>
         <input type="text" id="rating" name="rating"><br>
         <input type="hidden" id="id" name="id"><br>
