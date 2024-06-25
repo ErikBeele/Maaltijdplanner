a9b286e069b486bf7102aca7797369dc909e1321
Don332 <175736@student.horizoncollege.nl>
2024-06-25 09:49:03 +0200

    errors,typos gefixed

--- overzicht.php
+++ overzicht.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-
+<html>
 <head>
 <title>Maaltijdplanner</title>
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="navbar.css">
 <link rel="stylesheet" href="form.css">
 </head>
-
 <body>
     <div class="box">
    <header>
@@ -22,7 +21,7 @@
    <table class="toegevoegd">
     <tr>
         <th>titel</th>
-        <th>gerecht</th>
+        <th>type</th>
         <th>ingrediënt 1</th>
         <th>ingrediënt 2</th>
         <th>ingrediënt 3</th>
@@ -34,43 +33,42 @@
     </tr>
 
    <?php
-
-    $bestelling = $_POST['gerecht'];
-    $aantal = $_POST['num-gerechten'];
+    $bestelling = $_POST['gerecht'] ?? [];
+    $aantal = $_POST['num-gerechten'] ?? 0;
 
     $dbhost = "localhost";
     $dbname = "maaltijdplanner";
     $dbuser = "bit_academy";
     $dbpass = "bit_academy";
 
-try { 
-    $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try { 
+        $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare("SELECT * FROM gerechten ");
-    $stmt->execute(); 
+        $stmt = $conn->prepare("SELECT * FROM gerechten");
+        $stmt->execute(); 
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-       
-        print_r($_POST);
+        $gerechten = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach ($bestelling as $key1 => $gerecht) {
-            foreach ($gerecht as $key2 => $maaltijd) {
-                echo $maaltijd;
+        foreach ($gerechten as $row) {
+            if (in_array($row['type'], $bestelling)) {
+                echo "<tr>";
+                echo "<td>" . htmlspecialchars($row['titel']) . "</td>";
+                echo "<td>" . htmlspecialchars($row['type']) . "</td>";
+                for ($i = 1; $i <= 6; $i++) {
+                    echo "<td>" . htmlspecialchars($row["ingrediënt$i"]) . "</td>";
+                }
+                echo "<td>" . htmlspecialchars($row['tijd']) . "</td>";
+                echo "<td>" . htmlspecialchars($row['rating']) . "</td>";
+                echo "</tr>";
             }
-        } 
-        foreach ($aantal as $key3 => $aan) {
-            foreach ($aan as $key4 => $hoeveel) {
-                echo $hoeveel;
-            }
-        } 
+        }
+    } catch (PDOException $err) {
+        echo "Database couldn't reach the connection. " . $err->getMessage();
+        exit(); 
     }
-} catch (PDOException $err) {
-    echo "Database couldn't reach the connection. " . $err->getMessage() ;
-    exit(); 
-}
-?>
+    ?>
 
-</table>
+    </table>
 </body>
 </html>
