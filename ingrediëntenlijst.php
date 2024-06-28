3f65a14985a4327437ee86b7d9d7acad288b1ac7
ErikBeelen <eriklbeelen@gmail.com>
2024-06-28 11:30:53 +0200

    comments ingrediëntenlijst + navbar
    type: update
    scope: ingrediëntenlijst.php
    description: Code van ingrediëntenlijst.php uitgelegd met comments + navbar toegevoegd.

--- ingrediëntenlijst.php
+++ ingrediëntenlijst.php
@@ -1,10 +1,30 @@
+<!DOCTYPE html>
+<!--Dit zorgt ervoor dat de linkjes worden gekoppeld met de juiste pagina's--->
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="navbar.css">
+<link rel="stylesheet" href="form.css">
+</head>
+<body>
+    <div class="box">
+   <header>
+    <!--Dit is de navigatiebalk --->
+    <nav>
+        <ul>
+            <li><a href="toevoegen.php">Toevoegen</a></li>
+            <li><a href="index.php">Kiezen</a></li>
+            <li><a href="ingrediëntenlijst.php">ingrediëntenlijst</a></li>
+        </ul>
+    </nav>
+   </header>
+
 <?php 
-    
+    //Informatie voor connectie met database
     $dbhost = "localhost";
     $dbname = "maaltijdplanner";
     $dbuser = "bit_academy";
     $dbpass = "bit_academy";
 
+    //Connectie met database
 try { 
     $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -12,6 +32,7 @@ try {
     $stmt = $conn->prepare("SELECT * FROM gerechten");
     $stmt->execute(); 
 
+    //De juiste ingrediënten en het juiste aantal(6) ingrediënten worden geprind. Als er geen gerechten zijn gevonden krijg je het bericht "Geen gerechten gevonden".
     $gerechten = $stmt->FetchAll(PDO::FETCH_ASSOC);
     
     if($gerechten) {
@@ -33,10 +54,6 @@ try {
     echo "Database connection failed: " . $err->getMessage();
     exit();
 }
-
-
 ?>
-
-<body>
-    <li><a href="index.php">terug naar main page</a></li>
-</body>
+    </div>
+</html>
